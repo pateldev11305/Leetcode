9e1e91e9e2ff3b01410125fa14c78e3e72bbc62e
Dev Patel <161505408+pateldev11305@users.noreply.github.com>
2025-12-25 01:06:47 +0530

    Time: 33 ms (23.67%), Space: 31.4 MB (5.31%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
@@ -1,20 +1,17 @@
 class Solution:
-    def minimumDeletions(self, nums):
-        n = len(nums)
-        min_index = 0
-        max_index = 0
+    def minimumDeletions(self, nums: List[int]) -> int:
+        lowest=0
+        highest=0
+        for i in range(len(nums)):
+            if nums[i]<nums[lowest]:
+                lowest=i
+            if nums[i]>nums[highest]:
+                highest=i
 
-        # Find indices of min and max elements
-        for i in range(n):
-            if nums[i] > nums[max_index]:
-                max_index = i
-            if nums[i] < nums[min_index]:
-                min_index = i
-
-        # Calculate deletions from different sides
-        delete_from_front = max(max_index, min_index) + 1
-        delete_from_back = n - min(max_index, min_index)
-        delete_from_both_side = (min(max_index, min_index) + 1) + (n - max(max_index, min_index))
-
-        # Return minimum of all options
-        return min(delete_from_both_side, delete_from_front, delete_from_back)
+        length=len(nums)-1
+        leftL=lowest-0
+        rightL=length-lowest
+        leftH=highest-0
+        rightH=length-highest
+        total = min(max(leftL,leftH)+1,max(rightL,rightH)+1,(leftL+1)+(rightH+1),(leftH+1)+(rightL+1))
+        return total
