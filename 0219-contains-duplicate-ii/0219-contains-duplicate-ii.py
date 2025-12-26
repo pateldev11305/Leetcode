51297877e4bd626a8176c6f0fe6b7f00fa31d6b0
Dev Patel <161505408+pateldev11305@users.noreply.github.com>
2025-12-26 19:46:56 +0530

    Time: 15 ms (99.42%), Space: 30.1 MB (96.03%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,11 +1,24 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        seen = {}
 
-        for i, val in enumerate(nums):
-            if val in seen and i - seen[val] <= k:
+        if len(set(nums)) == len(nums):
+            return False
+
+        window_map = set()
+
+        left = 0
+
+        for right in range(len(nums)):
+
+            if right - left > k:
+                window_map.remove(nums[left]) # remove oldest element from window
+                left += 1
+            
+            if nums[right] in window_map:
                 return True
-            else:
-                seen[val] = i
+
+            window_map.add(nums[right]) 
         
         return False
+
+        
