2e138806cafb66691a3a422f51fa841767b0293c
Dev Patel <161505408+pateldev11305@users.noreply.github.com>
2025-12-28 15:19:53 +0530

    Time: 5 ms (50.72%), Space: 18.6 MB (85.9%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,8 +4,7 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         fPtr=0
-        hSet=set(nums)
-        if 0 in hSet:
+        if 0 in nums:
             for i in range(len(nums)):
                 if nums[i]!=0:
                     nums[fPtr],nums[i]=nums[i],nums[fPtr]
