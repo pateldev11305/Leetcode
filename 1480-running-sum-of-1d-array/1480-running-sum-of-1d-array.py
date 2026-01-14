9e6c4d5f5b860ae4bff604b23dc420e52966a610
Dev Patel <161505408+pateldev11305@users.noreply.github.com>
2026-01-14 21:13:21 +0530

    Time: 0 ms (100%), Space: 19.4 MB (5.88%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -1,9 +1,7 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         sum=0
-        #res=[]
         for i in range(len(nums)):
             sum+=nums[i]
             nums[i]=sum
-            #res.append(sum)
         return nums
